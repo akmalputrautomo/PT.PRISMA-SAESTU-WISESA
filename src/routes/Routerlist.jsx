877f9c24096b1f939da3b68ec8def5e8e16bca5f
akmalputrautomo <apu.utomo@gmail.com>
2025-03-02 16:20:24 +0700

    feat: :fire: News
    News

--- src/routes/Routerlist.jsx
+++ src/routes/Routerlist.jsx
@@ -1,26 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
-<<<<<<< HEAD
-import { HomePage } from "../pages/HomePage";
-import { SekilasPerusahaan } from "../pages/TentangKami/SekilasPerusahaan";
-import { VisiMisi } from "../pages/TentangKami/VisiMisi";
-import { BoardOfDicertor } from "../pages/TentangKami/BoardOfDicertor";
-import { MengapaMemilihKami } from "../pages/TentangKami/MengapaMemilihKami";
-import { Produk1 } from "../pages/Produk/Produk1";
-import { Produk2 } from "../pages/Produk/Produk2";
-import { Produk3 } from "../pages/Produk/Produk3";
-import { Fasilitas } from "../pages/Fasilitas";
-import { Karir } from "../pages/Karir";
-import { News } from "../pages/News";
-import { Footer } from "../Component/Footer";
-import { DetailNews } from "../pages/DetailNews";
-=======
+import { DetailNews } from "@/pages/DetailNews";
 import { HomePage } from "@/pages/HomePage";
 import { SekilasPerusahaan } from "@/pages/TentangKami/SekilasPerusahaan";
 import { VisiMisi } from "@/pages/TentangKami/VisiMisi";
 import { BoardOfDicertor } from "@/pages/TentangKami/BoardOfDicertor";
 import { MengapaMemilihKami } from "@/pages/TentangKami/MengapaMemilihKami";
-
 import { Fasilitas } from "@/pages/Fasilitas";
 import { Karir } from "@/pages/Karir";
 import { News } from "@/pages/News";
@@ -28,7 +13,6 @@ import Navbar from "@/Components/navbar";
 import { Footer } from "@/Components/Footer";
 import ProdukDetail from "@/pages/Produk/ProdukDetail";
 import { Produk } from "@/pages/Produk/Produk";
->>>>>>> 4953b87d7d06b40ba9260aba827664aa6a080a93
 
 export const Routerlist = () => {
   return (
@@ -39,19 +23,10 @@ export const Routerlist = () => {
         <Route path="/" element={<HomePage />} />
 
         {/* Tentang Kami */}
-        <Route
-          path="/TentangKami/SekilasPerusahaan"
-          element={<SekilasPerusahaan />}
-        />
+        <Route path="/TentangKami/SekilasPerusahaan" element={<SekilasPerusahaan />} />
         <Route path="/TentangKami/VisiMisi" element={<VisiMisi />} />
-        <Route
-          path="/TentangKami/BoardOfDirector"
-          element={<BoardOfDicertor />}
-        />
-        <Route
-          path="/TentangKami/MengapaMemilihKami"
-          element={<MengapaMemilihKami />}
-        />
+        <Route path="/TentangKami/BoardOfDirector" element={<BoardOfDicertor />} />
+        <Route path="/TentangKami/MengapaMemilihKami" element={<MengapaMemilihKami />} />
 
         {/* Produk */}
         <Route path="/produk" element={<Produk />} />
